ef410a89c7766105b0991abf7bfb239d1e1480ce
shan6321 <iamshan6321@gmail.com>
2024-11-30 23:26:07 +0530

    new date file

--- src/main/java/com/onlinevote/utils/DateTimeUtil.java
+++ src/main/java/com/onlinevote/utils/DateTimeUtil.java
@@ -1,2 +1,22 @@
-package com.onlinevote.utils;public class DateTimeUtil {
+package com.onlinevote.utils;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
+public class DateTimeUtil {
+
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+    public static String getFormattedUtcDateTime(ZoneId zoneId) {
+        log.info("===Inside getFormattedUtcDateTime Method===");
+        ZonedDateTime userCurrentDateTime = ZonedDateTime.now(zoneId);
+        log.info("userCurrentDateTime: " + userCurrentDateTime);
+        ZonedDateTime utcDateTime = userCurrentDateTime.withZoneSameInstant(ZoneOffset.UTC);
+        log.info("utcDateTime: " + utcDateTime);
+        log.info("utcDateTime1: " + utcDateTime.format(FORMATTER));
+        return utcDateTime.format(FORMATTER);
+    }
 }
